a76c0f6a444233a1d0c2204e9ff8d1493c0a0e61
Nguyễn Đình Hải <nguyenhai220101@gmail.com>
2024-11-20 17:05:14 +0700

    Add files via upload

---
+++ BAI1/RS232/Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿
+namespace RS232
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnDisConn = new System.Windows.Forms.Button();
+            this.btnConn = new System.Windows.Forms.Button();
+            this.txtConn = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.buttstop = new System.Windows.Forms.Button();
+            this.btnOff = new System.Windows.Forms.Button();
+            this.btnOn = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Blue;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(466, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = " Control 3 devices ( Team 1) ";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.btnDisConn);
+            this.groupBox1.Controls.Add(this.btnConn);
+            this.groupBox1.Controls.Add(this.txtConn);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Font = new System.Drawing.Font("Arial Narrow", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(37, 21);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(236, 150);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Commuication Setup";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Arial Narrow", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(6, 29);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(61, 17);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "COM Port";
+            // 
+            // btnDisConn
+            // 
+            this.btnDisConn.Font = new System.Drawing.Font("Arial Narrow", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDisConn.Location = new System.Drawing.Point(138, 110);
+            this.btnDisConn.Name = "btnDisConn";
+            this.btnDisConn.Size = new System.Drawing.Size(78, 25);
+            this.btnDisConn.TabIndex = 3;
+            this.btnDisConn.Text = "Disconnect";
+            this.btnDisConn.UseVisualStyleBackColor = true;
+            this.btnDisConn.Click += new System.EventHandler(this.btnDisConn_Click);
+            // 
+            // btnConn
+            // 
+            this.btnConn.Font = new System.Drawing.Font("Arial Narrow", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConn.Location = new System.Drawing.Point(25, 110);
+            this.btnConn.Name = "btnConn";
+            this.btnConn.Size = new System.Drawing.Size(82, 25);
+            this.btnConn.TabIndex = 2;
+            this.btnConn.Text = "Connect";
+            this.btnConn.UseVisualStyleBackColor = true;
+            this.btnConn.Click += new System.EventHandler(this.btnConn_Click);
+            // 
+            // txtConn
+            // 
+            this.txtConn.BackColor = System.Drawing.Color.LightCoral;
+            this.txtConn.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtConn.Font = new System.Drawing.Font("Arial Narrow", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtConn.ForeColor = System.Drawing.Color.Black;
+            this.txtConn.Location = new System.Drawing.Point(79, 68);
+            this.txtConn.Name = "txtConn";
+            this.txtConn.ReadOnly = true;
+            this.txtConn.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtConn.Size = new System.Drawing.Size(100, 22);
+            this.txtConn.TabIndex = 1;
+            this.txtConn.Text = "Disconnected!";
+            this.txtConn.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.Font = new System.Drawing.Font("Arial Narrow", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(73, 21);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(131, 30);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.buttstop);
+            this.groupBox2.Controls.Add(this.btnOff);
+            this.groupBox2.Controls.Add(this.btnOn);
+            this.groupBox2.Controls.Add(this.pictureBox1);
+            this.groupBox2.Font = new System.Drawing.Font("Arial Narrow", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.groupBox2.Location = new System.Drawing.Point(279, 27);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(184, 144);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Control LED1";
+            this.groupBox2.Enter += new System.EventHandler(this.groupBox2_Enter);
+            // 
+            // buttstop
+            // 
+            this.buttstop.Font = new System.Drawing.Font("Arial Narrow", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttstop.Location = new System.Drawing.Point(96, 104);
+            this.buttstop.Name = "buttstop";
+            this.buttstop.Size = new System.Drawing.Size(74, 24);
+            this.buttstop.TabIndex = 5;
+            this.buttstop.Text = "STOP";
+            this.buttstop.UseVisualStyleBackColor = true;
+            this.buttstop.Click += new System.EventHandler(this.buttstop_Click);
+            // 
+            // btnOff
+            // 
+            this.btnOff.Font = new System.Drawing.Font("Arial Narrow", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOff.Location = new System.Drawing.Point(96, 65);
+            this.btnOff.Name = "btnOff";
+            this.btnOff.Size = new System.Drawing.Size(74, 24);
+            this.btnOff.TabIndex = 4;
+            this.btnOff.Text = "CLOSE";
+            this.btnOff.UseVisualStyleBackColor = true;
+            this.btnOff.Click += new System.EventHandler(this.btnOff_Click);
+            // 
+            // btnOn
+            // 
+            this.btnOn.Font = new System.Drawing.Font("Arial Narrow", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOn.Location = new System.Drawing.Point(96, 21);
+            this.btnOn.Name = "btnOn";
+            this.btnOn.Size = new System.Drawing.Size(74, 29);
+            this.btnOn.TabIndex = 3;
+            this.btnOn.Text = "OPEN";
+            this.btnOn.UseVisualStyleBackColor = true;
+            this.btnOn.Click += new System.EventHandler(this.btnOn_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::RS232.Properties.Resources.mo;
+            this.pictureBox1.Location = new System.Drawing.Point(6, 23);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(84, 106);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 27F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(466, 176);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Arial Narrow", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "RS232(COM) Comunication lab";
+            this.TopMost = true;
+            this.TransparencyKey = System.Drawing.SystemColors.Info;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtConn;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Button btnDisConn;
+        private System.Windows.Forms.Button btnConn;
+        private System.Windows.Forms.Button btnOff;
+        private System.Windows.Forms.Button btnOn;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label6;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button buttstop;
+    }
+}
+
---
+++ BAI1/RS232/Form1.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.IO.Ports;
+
+namespace RS232
+{
+    public partial class Form1 : Form
+    {
+        string ReceiveData = String.Empty;
+        string TransmitData = String.Empty;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] ports = SerialPort.GetPortNames();
+
+            foreach (string port in ports)
+            {
+                comboBox1.Items.Add(port);
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            serialPort1.PortName = comboBox1.Text;
+
+        }
+
+
+        // nut ket noi com
+        private void btnConn_Click(object sender, EventArgs e)
+        {
+            
+            if (comboBox1.Text == "")
+            {
+                MessageBox.Show("Select COM Port.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                try
+                {
+                    if (serialPort1.IsOpen)
+                    {
+                        MessageBox.Show(" COM Ports is connected and ready for use ", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else
+                    {
+                        serialPort1.Open();
+                        txtConn.BackColor = Color.Lime;
+                        txtConn.Text = " Connecting...";
+                        comboBox1.Enabled = false;
+                    }
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("COM port is not found. Please check your COM or Cable.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+
+            }
+
+        }
+
+
+        // nut ngat ket noi com
+        private void btnDisConn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (serialPort1.IsOpen)
+                {
+                    serialPort1.Close();
+                    txtConn.BackColor = Color.Red;
+                    txtConn.Text = " Disconnected !";
+                    comboBox1.Enabled = true;
+
+                }
+                else
+                {
+                    MessageBox.Show(" COM Ports is disconnected and ready for use ", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("COM port is not found. Please check your COM or Cable.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+        // nut OPEN
+        private void btnOn_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                if (serialPort1.IsOpen)
+                {
+                    TransmitData = "a";
+                    serialPort1.Write(TransmitData);
+
+                }
+                else
+                {
+                    MessageBox.Show(" COM Ports is disconnected and ready for use ", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("COM port is not found. Please check your COM or Cable.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // nut CLOSE
+        private void btnOff_Click(object sender, EventArgs e)
+        {
+
+            try
+
+            {
+                if (serialPort1.IsOpen)
+                {
+                    TransmitData = "d";
+                    serialPort1.Write(TransmitData);
+
+                }
+                else
+                {
+                    MessageBox.Show(" COM Ports is disconnected and ready for use ", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("COM port is not found. Please check your COM or Cable.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            ReceiveData = serialPort1.ReadExisting();
+            this.Invoke(new EventHandler(DoupDate));
+        }
+
+        private void DoupDate(object sender, EventArgs e)
+        {
+            if (ReceiveData == "s")
+            {
+                pictureBox1.Image = RS232.Properties.Resources.ledon;
+            }
+            else if (ReceiveData == "f")
+            {
+                pictureBox1.Image = RS232.Properties.Resources.ledoff;
+
+            }
+
+        }
+
+        // nut STOP
+        private void buttstop_Click(object sender, EventArgs e)
+        {
+            try
+
+            {
+                if (serialPort1.IsOpen)
+                {
+                    TransmitData = "d";
+                    serialPort1.Write(TransmitData);
+
+                }
+                else
+                {
+                    MessageBox.Show(" COM Ports is disconnected and ready for use ", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("COM port is not found. Please check your COM or Cable.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DialogResult answer = MessageBox.Show("Do you want to exits? ", "Question", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (answer == DialogResult.No)
+            {
+                e.Cancel = true;
+            }
+            else
+            {
+                if (serialPort1.IsOpen)
+                {
+                    serialPort1.Close();
+                }
+            }
+        }
+    }
+}
